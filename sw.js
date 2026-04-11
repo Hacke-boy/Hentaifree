45c39aa29f096a8cd6c60f29237743d248e6d605
Hacke-boy <ldorce30@gmail.com>
2026-04-11 02:29:06 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js - Gestion du cache pour Haïti
+const CACHE_NAME = 'psyx-manga-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap'
+];
+
+// Installation : Mise en cache des fichiers de base
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
